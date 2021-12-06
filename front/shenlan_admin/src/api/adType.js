aaf4e252eb27a851af8960fd07bf7a02ceea7339
Zfans <zzzfans@qq.com>
2021-12-06 21:46:06 +0800

    表单、列表

---
+++ front/shenlan_admin/src/api/adType.js
@@ -0,0 +1,54 @@
+import request from "@/utils/request";
+
+export default {
+
+  list() {
+    return request({
+      baseURL: "http://127.0.0.1:8140",
+      url: "/admin/cms/ad-type/list",
+      method: "get"
+    });
+  },
+
+  pageList(page, limit) {
+    return request({
+      baseURL: "http://127.0.0.1:8140",
+      url: `/admin/cms/ad-type/list/${page}/${limit}`,
+      method: "get"
+    });
+  },
+
+  removeById(id) {
+    return request({
+      baseURL: "http://127.0.0.1:8140",
+      url: `/admin/cms/ad-type/remove/${id}`,
+      method: "delete"
+    });
+  },
+
+  save(adType) {
+    return request({
+      baseURL: "http://127.0.0.1:8140",
+      url: "/admin/cms/ad-type/save",
+      method: "post",
+      data: adType
+    });
+  },
+
+  getById(id) {
+    return request({
+      baseURL: "http://127.0.0.1:8140",
+      url: `/admin/cms/ad-type/get/${id}`,
+      method: "get"
+    });
+  },
+
+  updateById(adType) {
+    return request({
+      baseURL: "http://127.0.0.1:8140",
+      url: "/admin/cms/ad-type/update",
+      method: "put",
+      data: adType
+    });
+  }
+};
